4e17c1dce032e0ce025c1881c99433d00bbf8baf
sanketsh4h <sanketshah5135@gmail.com>
2023-12-10 04:48:45 -0800

    added 25 seq

--- main.cu
+++ main.cu
@@ -54,10 +54,10 @@ int main(int argc, char** argv) {
     //     return 1;
     // }
 
-    const int width = 8;
-    const int height = 8;
-    char sequence1[height+1] = "AGTACGTA";
-    char sequence2[width+1] = "TATAGCGA";
+    const int width = 25;
+    const int height = 25;
+    char sequence1[height+1] = "TGATATAGCATTAGTCAGCGGAGAA";
+    char sequence2[width+1] = "GCATGTATTCCTGCATGTATACAAC";
     int scoreMatrix[width * height];
 
     // Initialize score matrix
